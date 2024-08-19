069cd9da780d4353c2c6462566f5d2f8fe46011e
uma <uma@tigeen.ch>
2024-08-19 15:15:11 +0530

    welcome text placed in center

--- source/screens/welcome.js
+++ source/screens/welcome.js
@@ -1,15 +1,21 @@
 import {StyleSheet, Text, View} from 'react-native';
+import {Theme} from '../Theme';
 
 const WelcomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Welcome</Text>
+      <Text style={styles.textStyle}>Welcome</Text>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+  },
+  textStyle: {
+    color: Theme.colors.black,
+    alignSelf: 'center',
   },
 });
 
